96d70fc0b53581517668972cb1302b0b96e5f190
Kit Ewbank <Kit.Ewbank@capitalone.com>
2015-06-16 11:04:41 -0400

    Some cleanup.

--- src/main/java/com/netsol/adagent/util/campaignanalysis/type/CampaignType.java
+++
@@ -1,7 +0,0 @@
-package com.netsol.adagent.util.campaignanalysis.type;
-
-public enum CampaignType {
-	ONLINE, GEO_TARGETED, GEO_LONGTAIL;
-	
-	public static String sccsId = "@(#) adagent-util_d16.16.0.latest 08/09/12 09:54:19 CampaignType.java NSI";
-}
